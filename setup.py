3563ff307acb255b2b25cb897f8a4150eff57dc7
Henry Wu <wufuheng@gmail.com>
2025-08-09 17:21:03 -0700

    init

---
+++ setup.py
@@ -0,0 +1,88 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the README file for long description
+def read_readme():
+    with open("README.md", "r", encoding="utf-8") as fh:
+        return fh.read()
+
+# Read requirements from requirements.txt
+def read_requirements():
+    with open("requirements.txt", "r", encoding="utf-8") as fh:
+        return [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="microgpt",
+    version="0.0.2.dev0",
+    author="Wu Fuheng",
+    author_email="wufuheng@gmail.com",
+    description="Lightweight GPT implementation designed for resource-constrained environments",
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/henrywoo/microgpt",
+    project_urls={
+        "Bug Tracker": "https://github.com/henrywoo/microgpt/issues",
+        "Documentation": "https://github.com/henrywoo/microgpt#readme",
+        "Source Code": "https://github.com/henrywoo/microgpt",
+    },
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    python_requires=">=3.8",
+    install_requires=read_requirements(),
+    extras_require={
+        "dev": [
+            "pytest>=7.0.0",
+            "pytest-cov>=4.0.0",
+            "black>=23.0.0",
+            "flake8>=6.0.0",
+            "mypy>=1.0.0",
+            "pre-commit>=3.0.0",
+        ],
+        "docs": [
+            "sphinx>=6.0.0",
+            "sphinx-rtd-theme>=1.2.0",
+            "myst-parser>=1.0.0",
+        ],
+        "examples": [
+            "matplotlib>=3.6.0",
+            "seaborn>=0.12.0",
+            "jupyter>=1.0.0",
+            "ipywidgets>=8.0.0",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "microgpt-train=microgpt.pretrain.clm_pretrain_v0:main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    keywords=[
+        "gpt",
+        "transformer",
+        "language-model",
+        "nlp",
+        "machine-learning",
+        "deep-learning",
+        "pytorch",
+        "lightweight",
+        "micro",
+        "resource-efficient",
+    ],
+    license="MIT",
+    platforms=["any"],
+)
